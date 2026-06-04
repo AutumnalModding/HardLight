bede30a4eb2e82a130bca9a452d4ff4c0e4771a8
Lily Autumn <realprismaticyt@gmail.com>
2026-06-04 14:04:33 +1200

    Engraving tools!

---
+++ Content.Server/_HL/Engraving/EngravingToolComponent.cs
@@ -0,0 +1,14 @@
+namespace Content.Shared._HL.Engraving;
+
+[RegisterComponent]
+public sealed partial class EngravingToolComponent : Component
+{
+    [DataField, ViewVariables]
+    public int MaxUses = 1;
+
+    [ViewVariables(VVAccess.ReadWrite)]
+    public int RemainingUses = -1;
+
+    [DataField]
+    public bool CanReengrave;
+}
